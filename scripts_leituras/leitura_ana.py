5d45c946bc22672d44ccae053bfa636061df1347
AnaReis-SPTech <ana.reis@sptech.school>
2026-08-21 19:21:49 -0300

    fix - corrige lógica de horas leitura_ana.py

--- scripts_leituras/leitura_ana.py
+++ scripts_leituras/leitura_ana.py
@@ -45,34 +45,42 @@ for i in range(len(ram)):
 
 
 
-# Média de uso de cpu da última hora
+# Média de uso de ram da última hora
 media = 0
 total = 0
 media_ram_hora = 0.0
 tempo_atual =  datetime.now()
-hora_atual =  tempo_atual.strftime("%H")
-ultima_hora = (int(hora_atual) - 1)
+ultima_hora = (int(tempo_atual.hour) - 1)
 pico_cpu = cpu[0]
 user_pico = user[0]
+
+param_seg = int(600) # 10 min
+param_seg_ram = int(3600) # 10 min
+hatual_em_segundos = int(tempo_atual.hour) * 3600
+matual_em_segundos = int(tempo_atual.minute) * 60
+segundos_atuais = int(tempo_atual.second)
+
+tempo_atual_s = hatual_em_segundos + matual_em_segundos + segundos_atuais
+restricao_tempo = tempo_atual_s - param_seg
+restricao_tempo_hr = tempo_atual_s - param_seg_ram
 
 
 for i in range(len(timestamp)):
 
-    if (timestamp[i].hour == ultima_hora) :
+    if (tempo_atual_s >= restricao_tempo_hr):
         media += ram[i]
         total += 1
 
 if not(media == 0.0):
-    media_hora = media / total
-
+    media_ram_hora = media / total
 
 
 # Pico de uso da CPU da última hora
 
 for i in range(len(timestamp)):
 
-    if (timestamp[i].hour == ultima_hora and pico_cpu <= cpu[i] ) :
-        user_pico = user[i - 1]
+    if (tempo_atual_s <= restricao_tempo_hr and pico_cpu <= cpu[i] ) :
+        user_pico = user[i]
         pico_cpu = cpu[i]
 
 
@@ -82,24 +90,15 @@ for i in range(len(timestamp)):
 # Nisso pega um for com if que pega só os minutos que eu quero até hora atual
 media_disco = 0
 qtd_dados = 0
-param_seg = int(600) # 10 min
-
-hatual_em_segundos = int(hora_atual) * 3600
-matual_em_segundos = int(tempo_atual.minute) * 60
-segundos_atuais = int(tempo_atual.second)
-
-tempo_atual_s = hatual_em_segundos + matual_em_segundos + segundos_atuais
-restricao_tempo = tempo_atual_s - param_seg
 
 for i in range(len(timestamp)):
 
     timestamp_h = int(timestamp[i].hour) * 3600
     timestamp_m = int(timestamp[i].minute) * 60
     timestamp_s = int(timestamp[i].second)
-
     timestamp_em_segundos = timestamp_h + timestamp_m + timestamp_s
 
-    if (timestamp_s >= restricao_tempo):
+    if (timestamp_em_segundos >= restricao_tempo):
         media_disco += disco[i]
         qtd_dados += 1
 
@@ -114,8 +113,8 @@ Pico de uso CPU ({ultima_hora}h): {pico_cpu:.2f}
 De: {user_pico}
 
 -- RAM --
-Média de uso ({ultima_hora}h): {media_ram_hora:.2f}
+Média de uso ({ultima_hora}hrs à {tempo_atual.hour}hrs): {media_ram_hora:.2f}
 
 -- Disco --
-Média de uso (últimos {int(param_seg / 60)}m): {media_disco}
+Média de uso (últimos {int(param_seg / 60)}m): {media_disco:.2f}
 """)
